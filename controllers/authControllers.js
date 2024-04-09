3fe11af881ce9cbdf7a0cf36a3fc4d124b111fff
anilsaka <anilsaka.ggs@hotmail.com>
2024-04-09 14:34:27 +0300

    yönetici girişi, token vs.

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,11 +1,33 @@
+const adminModel = require('../models/adminModel')
+const { responseReturn } = require('../utiles/response')
+const { createToken } = require('../utiles/tokenCreate')
+const bcrypt = require('bcrypt')
 class authControllers {
     admin_login = async (req, res) => {
-        console.log(req.body)
         const { email, password } = req.body
         try {
-
+            const admin = await adminModel.findOne({ email }).select('+password')
+            // console.log(admin)
+            if (admin) {
+                const match = await bcrypt.compare(password, admin.password)
+                // console.log(match)
+                if (match) {
+                    const token = await createToken({
+                        id: admin.id,
+                        role: admin.role
+                    })
+                    res.cookie('accessToken', token, {
+                        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+                    })
+                    responseReturn(res, 200, { token, message: 'Giriş işlemi başarılı!' })
+                } else {
+                    responseReturn(res, 404, { error: 'Girdiğiniz parola doğru değil' })
+                }
+            } else {
+                responseReturn(res, 404, { error: 'E-posta adresi bulunamadı' })
+            }
         } catch (error) {
-
+            responseReturn(res, 500, { error: error.message })
         }
     }
 }
